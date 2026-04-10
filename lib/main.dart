4fa9858261dcc67e3e94c8858b29392a12bf556a
Shodmonxoja <akbarovshodmonxoja@gmail.com>
2026-04-10 13:16:06 +0000

    added summary.dart and title_set.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Wikipedia Flutter'),
+        ),
+        body: const Center(
+          child: Text('Loading...'),
         ),
       ),
     );
+  }
+}
+
+class ArticleModel{
+  Future<Summary> getRandomArticleSummary() async{
+    final uri = Uri.https('en.wikipedia.org', '/api/rest_v1/page/random/summary');
+    final response = await get(uri);
+
+    if(response.statusCode != 200){
+      throw HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body));
   }
 }
